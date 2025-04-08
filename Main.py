0798c5f0655a1f45f83598cf784a67762a6b896a
Brendan Ng <brendanng88@gmail.com>
2025-04-07 20:39:17 -0700

    Made a little bit of progress

--- Main.py
+++ Main.py
@@ -1,25 +1,64 @@
 import pygame
 import sys
 import os
+from playerClass import Player
 
 
 #FOR BUTTONS, WE USING LUCIDA CALIGRAPHY FONT IN MICROSOFT PAINT
 
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+clicked_start = False
+
 pygame.init()
 os.environ['SDL_VIDEO_CENTERED'] = '1'
-screen = pygame.display.set_mode((2000, 1200))
-pygame.display.set_caption("Guess that Asian!")
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Fighting Game")
 clock = pygame.time.Clock()
+#------------------------------
+#STARTING SCREEN
 
-#TODO Put background image -> Load it first so everything else overrides -> (Images drawn in order of written)
-playButton = pygame.image.load("graphics/realPB.png")
+playButton = pygame.image.load("graphics/playButtons.png").convert_alpha()
+def draw_PB():
+    scaled_PB = pygame.transform.scale(playButton, (500,300))
+    screen.blit(scaled_PB, (250, 375))
+#------------------------------
+#BACKGROUND
+bg_image = pygame.image.load("graphics/castleBG.jpg").convert_alpha()
+def draw_bg():
+    scaledBG = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.blit(scaledBG, (0,0))
+#------------------------------
+#FIGHTERS
+
+player1 = Player(200, 375)
+player2 = Player(700, 375)
+
+#------------------------------
+x = pygame.Rect(50, 50, 300, 300)
+backupStart = pygame.draw.rect(screen, (255,0,0), x)
+
 running = True
 while running:
+    #--------------------------
+    #Drawing
+    draw_bg()
+    
+    if clicked_start == True:
+        player1.draw(screen)
+        player2.draw(screen)
+        player1.move()
+    while clicked_start != True:
+        draw_PB()
+    #--------------------------
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False 
-
-    screen.blit(playButton, (0,0)) #Draws my PlayButton
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+        if playButton.rect.collidepoint(mouse_pos):
+            clicked_start = True
     pygame.display.update()
     clock.tick(60) #Don't run more than 60 times a second
 
--- playerClass.py
+++ playerClass.py
@@ -1,6 +1,25 @@
-class player:
-    def __init__(self, username:str):
-        self.username = username
+import pygame
+
+class Player:
+    def __init__(self, x, y):
+        self.rect = pygame.Rect((x,y, 80, 180))
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, (255,0,0), self.rect)
+
+    #MOVEMENT
+    def move(self):
+        SPEED = 10
+        dx = 0
+        dy = 0
+
+        key = pygame.key.get_pressed()
+        if key[pygame.K_a]:
+            dx = -SPEED
+        if key[pygame.K_d]:
+            dx = SPEED
         
-    def get_username(username):
-        return username
+        #UPDATE POSITION
+        self.rect.x += dx #Left-Right
+        self.rect.y += dy #Jump
+
